e0fde1728c9278529baaa1a7b0873bdc14e11653
tsutsuimahitoDIVX <119909587+tsutsuimahitoDIVX@users.noreply.github.com>
2023-07-21 14:59:10 +0900

    security.config記述
    ホームの接続許可と、css許可

---
+++ src/main/java/in/techcamp/issueapp/SecurityConfig.java
@@ -0,0 +1,28 @@
+package in.techcamp.issueapp;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/css/**","/").permitAll()//ここに記載したパスは全てのユーザーに許可
+                        .anyRequest().authenticated())
+                .formLogin(login -> login
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/login?error")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/"));
+        return http.build();
+    }
+}
