dd6d8e63a8ba3b756c38823ee5bb71dffb1eccad
tsutsuimahitoDIVX <119909587+tsutsuimahitoDIVX@users.noreply.github.com>
2023-07-21 18:20:37 +0900

    ログインエラー時メッセージ表示の実装

--- src/main/java/in/techcamp/issueapp/controller/UserController.java
+++ src/main/java/in/techcamp/issueapp/controller/UserController.java
@@ -1,7 +1,9 @@
 package in.techcamp.issueapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -9,4 +11,13 @@ public class UserController {
     public String loginForm(){
         return "login";
     }
+
+    @GetMapping("/login")
+    public String login(@RequestParam(value = "error", required = false) String error, Model model) {
+        if (error != null) {
+            model.addAttribute("loginError", "名前かパスワードが間違っています。");
+        }
+        return "login";
+    }
 }
+
