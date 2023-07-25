c99e6a2f7a97bfe97245b684b53c3e0462ac3b6a
tsutsuimahitoDIVX <119909587+tsutsuimahitoDIVX@users.noreply.github.com>
2023-07-25 16:24:19 +0900

    更新機能実装

--- src/main/java/in/techcamp/issueapp/controller/IssueController.java
+++ src/main/java/in/techcamp/issueapp/controller/IssueController.java
@@ -6,6 +6,7 @@ import in.techcamp.issueapp.entity.UserEntity;
 import in.techcamp.issueapp.repository.IssueRepository;
 import in.techcamp.issueapp.repository.UserRepository;
 import in.techcamp.issueapp.service.IssueService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -13,10 +14,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
@@ -52,19 +50,56 @@ public class IssueController {
         return "redirect:/";
     }
 
-//    @GetMapping("/issues/{id}")
-//    public String issueDetail(@PathVariable long id, Model model){
-//        var issue = issueRepository.findById(id);
-//        model.addAttribute("issue", issue);
-//        return "detail";
-//    }
+//    イシュー更新機能（画面遷移）
+    @GetMapping("/user/{userId}/issue/{issueId}/edit")
+    public String edit(@PathVariable Integer userId, @PathVariable Integer issueId, Model model) {
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Account not found: " + userId));
+        IssueEntity issue = issueRepository.findById(issueId)
+                .orElseThrow(() -> new IllegalArgumentException("Memo not found: " + issueId));
 
-//    @PostMapping("/issues/{id}/update")
-//    public String updateIssue(@PathVariable long id, IssueForm issueForm){
-//        issueRepository.update(id, issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
-//        return "redirect:/";
-//    }
-//
+        model.addAttribute("user", user);
+        model.addAttribute("issue", issue);
+
+        return "detail"; // メモの編集画面へ遷移
+    }
+
+//    イシュー更新機能（更新ロジック）
+    @PostMapping("/user/{userId}/issue/{issueId}/update")
+    public String update(Authentication authentication,
+                         @PathVariable("userId") Integer userId,
+                         @PathVariable("issueId") Integer issueId,
+                         @RequestParam("title") String newTitle,
+                         @RequestParam("content") String newContent,
+                         @RequestParam("period") String newPeriod,
+                         @RequestParam("importance") char newImportance)
+    {
+    // 現在認証されているユーザー名を取得
+    String username = authentication.getName();
+
+    // 該当のイシューとアカウントを取得
+    IssueEntity issue = issueRepository.findById(issueId).orElseThrow(() -> new EntityNotFoundException("Memo not found: " + issueId));
+    UserEntity user = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("Memo not found: " + userId));
+
+    // ユーザーチェック
+    if (user.getUsername().equals(username)) {
+        // イシューを更新
+        issue.setTitle(newTitle);
+        issue.setContent(newContent);
+        issue.setPeriod(newPeriod);
+        issue.setImportance(newImportance);
+        issueRepository.save(issue);
+    }
+    else {
+        // エラーメッセージを設定したり、エラーページにリダイレクトしたりします。
+    }
+
+    // 更新後のページにリダイレクト
+
+    return "redirect:/";
+}
+
+
 //    @PostMapping("issues/{id}/delete")
 //    public String deleteIssue(@PathVariable long id) {
 //        issueRepository.deleteById(id);
