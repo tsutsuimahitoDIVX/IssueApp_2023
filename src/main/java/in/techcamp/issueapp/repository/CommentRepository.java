5d803463acedbe7e08008c8805c062059599f631
tsutsuimahitoDIVX <119909587+tsutsuimahitoDIVX@users.noreply.github.com>
2023-07-26 13:41:21 +0900

    リポジトリの作成

---
+++ src/main/java/in/techcamp/issueapp/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package in.techcamp.issueapp.repository;
+
+import in.techcamp.issueapp.entity.CommentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
+}
